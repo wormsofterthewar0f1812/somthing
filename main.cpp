6a79a7dbaa569db4ee18bf27c7e01c68b90881a4
wormsofterthewar0f1812 <zeea3906@psdblogs.ca>
2026-01-22 09:29:39 -0700

    Update main.cpp
    change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-using namspace std;
+using namespace std;
 
-int main() {
-    cout >> "worm testing idk";
-    return 0;
+void worms(){
+    cout << "void worms";
+}
+
+int main(){
+    for (int i = 1; i <= 5; i++){
+            cout << "worms" << endl; //shit
+    }
+    cout << "seth is mr clean" << endl; //shit
+    worms();
+    return(0);
 }
